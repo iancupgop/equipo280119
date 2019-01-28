30cf7003b9189a322ad20ff240f472eeffb78194
guillermo <guillermouser.iti@hotmail.com>
2019-01-28 09:38:51 -0600

    memo1 men
    listo

---
+++ proyectoEquipo/Memo1.swift
@@ -0,0 +1,9 @@
+//
+//  Memo1.swift
+//  proyectoEquipo
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 28/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import Foundation
