5b5bf1f59e39dbe83b99e92392e459e766e0246b
guillermo <guillermouser.iti@hotmail.com>
2019-01-30 09:19:55 -0600

    memoX

---
+++ proyectoEquipo/proyectoEquipo/memoX.swift
@@ -0,0 +1,9 @@
+//
+//  memoX.swift
+//  proyectoEquipo
+//
+//  Created by Universidad Politecnica de Gómez Palacio on 30/01/19.
+//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
+//
+
+import Foundation
