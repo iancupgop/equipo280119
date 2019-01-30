f24a86fc5d4c2312de42f2d40d196354d1523bb6
iancupgop <ian18charles@gmail.com>
2019-01-30 08:52:20 -0600

    Elimine

--- proyectoEquipo/proyectoEquipo/Ian1.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Ian1.swift
-//  proyectoEquipo
-//
-//  Created by Universidad Politecnica de Gómez Palacio on 1/30/19.
-//  Copyright © 2019 Universidad Politecnica de Gómez Palacio. All rights reserved.
-//
-
-import Foundation
